3d0dd2104a68cbebe58e0bf478e9f44fd45fa34d
36019Kuritagou <oibc36019@ojs.ac.jp>
2025-04-23 09:42:20 +0900

    インチからメートル変換完成

--- Chapter02/演習問題/Exercize02/Program.cs
+++ Chapter02/演習問題/Exercize02/Program.cs
@@ -2,6 +2,31 @@
     internal class Program {
         static void Main(string[] args) {
 
+         
+
+            for (int inch = 1; inch <= 10; inch++) {
+                double meter = InchToMeter(inch);
+                Console.WriteLine($"{inch}inch = {meter:0.0000}m");
+            }
+
+            for (int meter = 1; meter <= 10; meter++) {
+                double inch = MeterToInch(meter);
+                Console.WriteLine($"{meter}m = {inch:0.0000}inch");
+            }
+        }
+
+
+public static double InchToMeter(int inch) {
+            return inch * 0.0254;
+        }
+
+
+        public static double MeterToInch(int meter) {
+            return meter / 0.0254;
         }
     }
 }
+
+
+    
+
