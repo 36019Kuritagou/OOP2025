fc1d04e35c805ffcfad7591fc8a2d33cf65bb22f
36019Kuritagou <oibc36019@ojs.ac.jp>
2025-05-13 10:30:17 +0900

    2,1,1と2,1,2完了

---
+++ Chapter02/演習問題/Exercize01/Song.cs
@@ -0,0 +1,17 @@
+﻿namespace Exercize01 {
+    //2.1.1
+    public class Song {
+        public string Title { get; set; } = "歌のタイトル";
+        public string ArtistName { get; set; } = "アーティスト名";
+        public int Length { get; set; }
+    
+
+
+    public Song(string Title, string ArtistName, int Length) {
+            this.Title = Title;
+            this.ArtistName = ArtistName;
+            this.Length = Length;
+        }
+
+    }
+}
