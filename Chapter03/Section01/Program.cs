56447672d933c6bedd9a64d3a51f99677f0b43de
36019Kuritagou <oibc36019@ojs.ac.jp>
2025-05-16 14:02:50 +0900

    65まで

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -11,8 +11,8 @@ namespace Section01 {
             //Console.Write("カウントしたい数値:");
             //int num = int.Parse(Console.ReadLine());
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
-            Judgement judge = IsEven;
-            Console.WriteLine(Count(numbers,judge));
+
+            Console.WriteLine(Count(numbers, delegate (int n) { return n % 2 == 0; }));
 
         }
 
@@ -21,7 +21,7 @@ namespace Section01 {
             return n % 2 == 0;
         }
 
-        static int Count(int[] numbers, Judgement judge) {            
+        static int Count(int[] numbers, Predicate<int> judge) {            
             var count = 0;
             foreach (var n in numbers) {
                 //引数で受け取ったメソッドを呼び出す
