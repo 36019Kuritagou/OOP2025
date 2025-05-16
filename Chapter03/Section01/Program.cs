397eb1417c4c528abe2ad0db2f27a2f9c9a1ccad
36019Kuritagou <oibc36019@ojs.ac.jp>
2025-05-16 14:14:55 +0900

    以上以下

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -12,7 +12,7 @@ namespace Section01 {
             //int num = int.Parse(Console.ReadLine());
             var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
 
-            Console.WriteLine(Count(numbers, delegate (int n) { return n % 2 == 0; }));
+            Console.WriteLine(Count(numbers, n=> 5 <= n && n < 10));
 
         }
 
