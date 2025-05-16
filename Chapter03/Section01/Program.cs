eaecac2edc4cc8af547c89710a5904aee4371995
36019Kuritagou <oibc36019@ojs.ac.jp>
2025-05-16 15:59:15 +0900

    完成

--- Chapter03/Section01/Program.cs
+++ Chapter03/Section01/Program.cs
@@ -4,34 +4,26 @@ using System.Reflection.Metadata.Ecma335;
 namespace Section01 {
     internal class Program {
 
-        public delegate bool Judgement(int value);
-
         static void Main(string[] args) {
 
-            //Console.Write("カウントしたい数値:");
-            //int num = int.Parse(Console.ReadLine());
-            var numbers = new[] { 5, 3, 9, 6, 7, 5, 8, 1, 0, 5, 10, 4 };
+            var cities = new List<string> {
+                "Tokyo",
+                "New Delhi",
+                "Bangkok",
+                "London",
+                "Paris",
+                "Berlin",
+                "Canberra",
+                "Hong Kong",
+            };
 
-            Console.WriteLine(Count(numbers, n=> 5 <= n && n < 10));
+            var lowerList = cities.ConvAll(s => s.Length <= 5);
 
-        }
-
-        //メソッドへ渡す処理
-        static bool IsEven(int n) {
-            return n % 2 == 0;
-        }
-
-        static int Count(int[] numbers, Predicate<int> judge) {            
-            var count = 0;
-            foreach (var n in numbers) {
-                //引数で受け取ったメソッドを呼び出す
-                if (judge(n) == true) {
-                    count++;
-                }
+            foreach (var s in names) {
+                Console.WriteLine(s);
             }
-            return count;
-        }
 
+        }
     }
 }
 
---
+++ Chapter03/演習問題/Exercize02/Program.cs
@@ -0,0 +1,67 @@
+﻿
+namespace Exercize02 {
+    internal class Program {
+        static void Main(string[] args) {
+            var cities = new List<string> {
+                "Tokyo", "New Delhi", "Bangkok", "London",
+                "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            Console.WriteLine("***** 3.2.1 *****");
+            Exercise2_1(cities);
+            Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.2 *****");
+            Exercise2_2(cities);
+            Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.3 *****");
+            Exercise2_3(cities);
+            Console.WriteLine();
+
+            Console.WriteLine("***** 3.2.4 *****");
+            Exercise2_4(cities);
+            Console.WriteLine();
+
+        }
+
+        private static void Exercise2_1(List<string> cities) {
+            Console.WriteLine("都市名を入力。空行で終了");
+
+            string name;
+            do {
+                name = Console.ReadLine();
+
+                if (!string.IsNullOrEmpty(name)) {
+                    int index = cities.FindIndex(city => city == name);
+                    if (index >= 0) {
+                        Console.WriteLine(index);
+                    } else {
+                        Console.WriteLine(-1);
+                    }
+                }
+
+            } while (!string.IsNullOrEmpty(name));
+        }
+
+            
+        
+
+        private static void Exercise2_2(object names) {
+
+        }
+
+        private static void Exercise2_3(object names) {
+
+        }
+
+        private static void Exercise2_4(object names) {
+            
+        }
+
+       
+
+
+       
+    }
+}
