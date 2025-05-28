4137301addc84cba5ca5c6b7e54e888c1e014049
36019Kuritagou <oibc36019@ojs.ac.jp>
2025-05-28 09:44:30 +0900

    2つ以上見つかった場合完成

---
+++ chapter07/Section01/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Section01 {
+    internal class Program {
+        static void Main(string[] args) {
+
+            var books = Books.GetBooks();
+
+            //本の平均金額を表示
+            Console.WriteLine(books.Average(b => b.Price));
+
+            //本のページ合計を表示
+            Console.WriteLine(books.Sum(b => b.Pages));
+
+            //金額の安い書籍名と金額を表示            
+            var minprice = books.Min(b => b.Price);
+            var bo = books.Where(b => b.Price == minprice);
+            foreach (var b in bo) {
+                Console.WriteLine("金額の安い書籍 :" + b.Title + "   金額 :" + b.Price);
+            }
+
+            //ページが多い書籍名とページ数を表示
+            var maxpages = books.Max(b => b.Pages);
+            var bp = books.Where(b => b.Pages == maxpages);
+            foreach (var bb in bp) {
+                Console.WriteLine("ページが多い書籍名 :" + bb.Title + "   ページ数" + bb.Pages);
+            }
+
+            //タイトルに「物語」が含まれている書籍名をすべて表示
+            var titles = books.Where(b => b.Title.Contains("物語"));
+            foreach (var t in titles) {
+                Console.WriteLine(t.Title);
+            }
+            
+        }
+    }
+}
