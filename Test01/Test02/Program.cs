fe7a79e922a4070466f4ff5b37864784d45b692c
36019Kuritagou <oibc36019@ojs.ac.jp>
2025-06-17 12:07:49 +0900

    設問2_提出

--- Test01/Test02/Program.cs
+++ Test01/Test02/Program.cs
@@ -73,28 +73,33 @@ namespace Test02 {
         //問題３　昇順に並べて表示（遅延実行とする）
         //　　　　出力結果【12 14 17 20 31 35 40 48 53 76 87 91 94】
         private static void Exercise03(int[] numbers) {
-            
 
+           // var cou = numbers.OrderBy(n => );
         }
 
         //問題４　10以上50以下の数字のみを表示（即時実行でも可とする）
         //　　　　出力結果【12 14 20 40 35 31 17 48】
         private static void Exercise04(int[] numbers) {
-            
+            var number = numbers.Where(n => 10 >= 50).ToList();
+            foreach(var n in number) {
+                Console.WriteLine(n);
+            }
 
         }
 
         //問題５　Countメソッドを使い、小文字の'n'が含まれている都市名がいくつあるかカウントして結果を表示
         //　　　　出力結果【5】
         private static void Exercise05(List<string> cities) {
-            
+            var count = cities.Count(s => s.Contains('n'));
+            Console.WriteLine(count);
 
         }
 
         //問題６　全都市数
         //　　　　出力結果【8】
         private static void Exercise06(List<string> cities) {
-            
+            var count = cities.Count();
+            Console.WriteLine(count);
 
         }
 
@@ -109,7 +114,7 @@ namespace Test02 {
         //          Paris
         //          Tokyo】
         private static void Exercise07(List<string> cities) {
-            
+            Console.WriteLine(cities);
 
         }
 
@@ -152,7 +157,8 @@ namespace Test02 {
         //        【London
         //          Berlin】
         private static void Exercise10(List<string> cities) {
-            
+            var count = cities.Find(s => s.Length == 6);
+            Console.WriteLine(count);
 
 
         }
