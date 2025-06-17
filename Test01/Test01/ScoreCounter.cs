de68e936a837eb2db70e1087070565f03d08af4a
36019Kuritagou <oibc36019@ojs.ac.jp>
2025-06-17 11:18:04 +0900

    設問1_Studentクラス完成

---
+++ Test01/Test01/ScoreCounter.cs
@@ -0,0 +1,37 @@
+﻿namespace Test01 {
+    public class ScoreCounter {
+        private IEnumerable<Student> _score;
+        
+
+        // コンストラクタ
+        public ScoreCounter(string filePath) {
+             ;
+
+        }
+
+        //メソッドの概要： 
+        private static IEnumerable<Student> ReadScore(string filePath) {
+            
+
+
+
+
+        }
+
+        //メソッドの概要： 
+        public IDictionary<string, int> GetPerStudentScore() {
+            
+
+
+
+
+        }
+
+        Student student = new Student() {
+            Name = items[0],
+            Subject = items[1],
+            Score = int.Parse(items[2])
+        };
+
+    }
+}
