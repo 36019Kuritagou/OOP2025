4f17cf2680ae923f65fe9d3f44bf3b0c0e46b553
36019Kuritagou <oibc36019@ojs.ac.jp>
2025-06-27 10:28:07 +0900

    01完成

---
+++ chapter09/演習問題/Exercize01/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Globalization;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Exercize01 {
+    internal class Program {
+        static void Main(string[] args) {
+            var dateTime = DateTime.Now;
+            DisplayDatePattern1(dateTime);
+            DisplayDatePattern2(dateTime);
+            DisplayDatePattern3(dateTime);
+        }
+
+        private static void DisplayDatePattern1(DateTime dateTime) {
+            var form = string.Format("{0:yyyy/MM/dd mm:ss}", dateTime);
+            Console.WriteLine(form);
+        }
+
+        private static void DisplayDatePattern2(DateTime dateTime) {
+            var dform = dateTime.ToString($"yyyy年MM月dd日 mm:ss");
+            Console.WriteLine(dform);
+
+        }
+
+        private static void DisplayDatePattern3(DateTime dateTime) {
+            var date = new DateTime(2024, 3, 9);
+            var culture = new CultureInfo("ja-JP");
+            culture.DateTimeFormat.Calendar = new JapaneseCalendar();
+            var era = culture.DateTimeFormat.Calendar.GetEra(date);
+            var eraName = culture.DateTimeFormat.GetEraName(era);
+            var str = dateTime.ToString("ggyy年M月d日dddd", culture);
+            Console.WriteLine(str);
+        }
+    }
+}
