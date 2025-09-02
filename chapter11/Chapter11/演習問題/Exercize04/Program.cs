7df8e202ae8a182a92a88261f654efd8c8f8bf33
36019Kuritagou <oibc36019@ojs.ac.jp>
2025-09-02 16:00:50 +0900

    sample

---
+++ chapter11/Chapter11/演習問題/Exercize04/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Exercize04 {
+    internal class Program {
+        static void Main(string[] args) {
+            var lines = File.ReadAllLines("sample.txt");
+
+            var newlines = lines.Select(line => Regex.Replace(line, @"v4\.0""", @"v5.0"));
+
+            File.WriteAllLines("sampleChange.txt", newlines);
+
+            var text = File.ReadAllText("sampleChange.txt");
+            Console.WriteLine(text);
+        }
+    }
+}
+    
+
---
+++ chapter11/Section04/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Section04 {
+    internal class Program {
+        static void Main(string[] args) {
+            var lines = File.ReadAllLines("sample.txt");
+
+            var newlines =
+
+                File.WriteAllLines("sampleChange.txt", newlines);
+
+            var text = File.ReadAllText("sampleChange.txt");
+            Console.WriteLine(text);
+        }
+    }
+}
