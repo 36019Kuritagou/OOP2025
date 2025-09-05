f598997893670376b4973a4ff5b09b1fedde8fea
36019Kuritagou <oibc36019@ojs.ac.jp>
2025-09-05 10:34:34 +0900

    WPFアプリのUI構築

--- WPF/SampleApplication/App.xaml.cs
+++ WPF/SampleApplication/App.xaml.cs
@@ -6,12 +6,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace SampleApplication
-{
+namespace SampleApplication {
     /// <summary>
     /// App.xaml の相互作用ロジック
     /// </summary>
-    public partial class App : Application
-    {
+    public partial class App : Application {
     }
 }
