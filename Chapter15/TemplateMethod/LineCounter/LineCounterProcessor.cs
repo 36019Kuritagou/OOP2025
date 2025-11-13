01a2e26d9b5d91f171ca2552c046a4d3b1d307c6
36019Kuritagou <oibc36019@ojs.ac.jp>
2025-11-13 15:14:31 +0900

    kannsei

---
+++ Chapter15/TemplateMethod/LineCounter/LineCounterProcessor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TextFileProcessor;
+
+namespace LineCounter {
+    internal class LineCounterProcessor : TextProcessor {
+        private int _count = 0;
+        private string _keyword = "";
+
+        protected override void Initialize(string fname) {
+            Console.Write("カウントする単語を入力してください: ");
+            _keyword = Console.ReadLine() ?? "";
+            _count = 0;
+        }
+
+        protected override void Execute(string line) {
+            // 単語の出現回数をカウント（大文字小文字を区別しない）
+            _count += line.Split(new[] { ' ', '\t', ',', '.', ';', '(', ')', '{', '}', '[', ']' }, StringSplitOptions.RemoveEmptyEntries)
+                          .Count(word => string.Equals(word, _keyword, StringComparison.OrdinalIgnoreCase));
+        }
+
+        protected override void Terminate() {
+            Console.WriteLine($"\"{_keyword}\" の出現回数: {_count} 回");
+        }
+    }
+}
---
+++ Chapter15/TemplateMethod/LineCounter/Program.cs
@@ -0,0 +1,23 @@
+﻿using TextFileProcessor;
+
+namespace LineCounter {
+    internal class Program {
+        static void Main(string[] args) {
+            string filePath;
+
+            if (args.Length > 0) {
+                filePath = args[0];
+            } else {
+                Console.Write("処理するファイルのパスを入力してください: ");
+                filePath = Console.ReadLine() ?? "";
+            }
+
+            if (!File.Exists(filePath)) {
+                Console.WriteLine("指定されたファイルが存在しません。");
+                return;
+            }
+
+            TextProcessor.Run<LineCounterProcessor>(filePath);
+        }
+    }
+}
