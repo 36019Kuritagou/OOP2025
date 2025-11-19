6e8532bf76d35ab13896ecf14f1f21ec5e092e9d
36019Kuritagou <oibc36019@ojs.ac.jp>
2025-11-19 09:58:05 +0900

    完成

---
+++ Chapter15/TemplateMethod/TextFileProcessorDI/LineToHalfNumberService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextFileProcessorDI {
+    //問題15.1
+    internal class LineToHalfNumberService : ITextFileService {
+        private int _count;
+
+        public void Initialize(string fname) {
+        }
+
+        public void Execute(string line) {
+            Console.WriteLine(line.Normalize(NormalizationForm.FormKD));
+        }
+
+        public void Terminate() {
+            Console.WriteLine("終了");
+        }
+    }
+}
